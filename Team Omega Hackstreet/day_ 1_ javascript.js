87bbbed4b7382184ebca11b3b666a60cf50de82e
Yash Katke <85817393+yashkatke@users.noreply.github.com>
2023-09-14 17:59:24 +0530

    Add files via upload

---
+++ Team Omega Hackstreet/day_ 1_ javascript.js
@@ -0,0 +1,15 @@
+let User = {
+    name: "John"
+
+        phone: "123-456-7850",
+    address: {
+        streeet: "123 main st",
+        city: "New york",
+        state: "NY",
+        zip: "1001",
+    };
+    hobbies: ["movies", "music"],
+    magic_number: function(x) {
+        return x = x;
+    },
+};
